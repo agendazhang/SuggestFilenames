28bcf260cc537d59e46a5d72f5b9ad34b36b55a8
Yu Xiang <pyxelise@gmail.com>
2018-10-14 02:45:04 +0800

    Smthsmth

--- scripts.js
+++ scripts.js
@@ -1,3 +1,29 @@
+function get_prev_html_title() {
+    chrome.runtime.sendMessage({
+            call: "getPrevTitle",
+            url: document.URL
+        },
+        function (response) {
+            console.log(response);
+        });
+}
+
+// Doesn't work for .pdf files (only body tags appear)
+// EG: http://kmmc.in/wp-content/uploads/2014/01/lesson2.pdf
+// Q: Where does HTML title come from?
+function get_html_title() {
+    var titles = document.getElementsByTagName("title");
+    if (titles.length != 0) return titles[0].innerHTML;
+    return null;
+}
+
+function get_current_URL() {
+    var str_array = document.URL.split("/");
+    var name_pdf = str_array[str_array.length-1];
+    return name_pdf.substring(0, name_pdf.length-4);
+}
+
+
 var oneCount = 0;
 var twoCount = 0;
 var threeCount = 0;
@@ -45,33 +71,18 @@ function checkTicked3() {
     document.getElementById('textBox4').value = current;
 }
 
-function saveButtonClicked() {
-    var checkbox = document.getElementById('checkBox3');
-    var data = document.getElementById('textBox3').value;
-    var current = document.getElementById('textBox4').value;
-    if(threeCount % 2 == 0) {
-        current += data;
-        threeCount++;
-    }
-    else {
-        current = current.replace(data, "");
-        threeCount++;
-    }
-    document.getElementById('textBox4').value = current;
-}
+console.log("hi");
 
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('checkbox1').addEventListener('click', checkTicked1);
-});
-
-document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('checkbox2').addEventListener('click', checkTicked2);
-});
-
-document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('checkbox3').addEventListener('click', checkTicked3);
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('savebutton').addEventListener('click', saveButtonClicked);
+    chrome.runtime.sendMessage({
+            call: "getPrevTitle",
+            url: document.URL
+        },
+        function (response) {
+            console.log(response);
+            document.getElementById('checkbox1').value = response;
+        });
 });
